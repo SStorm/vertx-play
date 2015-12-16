6a1ca37581847297a1a56e20da5ca03bf9cff57a
Andrey Antonov <andrey.antonov@dataart.com>
2015-12-16 18:22:41 +0300

    ZOO-10155

--- src/main/java/com/alertme/test/Statistician.java
+++ src/main/java/com/alertme/test/Statistician.java
@@ -7,6 +7,8 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
+import static java.lang.String.format;
+
 public class Statistician extends AbstractVerticle {
 
     private Logger logger = LoggerFactory.getLogger(Statistician.class);
@@ -17,16 +19,16 @@ public class Statistician extends AbstractVerticle {
     public void start() {
         logger.info("Starting statistician");
         getVertx().eventBus().consumer("proxy.stats", handler -> {
-           if (handler.body() instanceof Integer) {
-               requestCount++;
-               long currentTime = System.currentTimeMillis();
-               if (currentTime - 10000 > lastStatsOutput) {
-                   double rps = requestCount / 10;
-                   logger.info("Requests processed: " + requestCount + ", " + rps + " rps");
-                   lastStatsOutput = currentTime;
-                   requestCount = 0;
-               }
-           }
+            if (handler.body() instanceof Integer) {
+                requestCount++;
+                long currentTime = System.currentTimeMillis();
+                if (currentTime - 10000 > lastStatsOutput) {
+                    double rps = requestCount / 10;
+                    logger.info(format("Requests processed: %s, %s rps", requestCount, rps));
+                    lastStatsOutput = currentTime;
+                    requestCount = 0;
+                }
+            }
         });
     }
 }
